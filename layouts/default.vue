00d88b2740387bd56b1aee92ecf79f7ed1848712
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-12-08 20:01:50 +0900

    delete unused components and page

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019 Shohei-Japan</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      fixed: false
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center"></div>
+      <v-card>
+        <v-card-title class="headline">
+          QR wi-fi
+        </v-card-title>
+        <v-card-text></v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
