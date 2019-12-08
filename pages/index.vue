fad9a868c1102763a870987b8fa1003286ebe2fd
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-12-09 03:10:34 +0900

    add canvas image

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,111 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center"></div>
-      <v-card>
-        <v-card-title class="headline">
-          QR wi-fi
-        </v-card-title>
-        <v-card-text></v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+    <h1 class="mb1">QR wi-fi</h1>
+    <v-card width="400">
+      <v-card-title>
+        wi-fi情報
+      </v-card-title>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="formData.ssid"
+                :rules="ssidRules"
+                label="SSID"
+                required
+              />
+              <v-select
+                v-model="formData.encryption"
+                :items="encryptionItems"
+                :rules="encryptionRules"
+                label="Encryption"
+              />
+              <v-text-field
+                v-model="formData.key"
+                :rules="keyRules"
+                label="KEY"
+                required
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+      <p v-if="showErrorMessage" class="text-center">
+        Woops, all forms must be filled.
+      </p>
+      <v-card-actions class="d-flex align-end flex-column">
+        <v-btn
+          @click="createImage()"
+          class="text-right"
+          outlined
+          color="primary"
+        >
+          Get QRCode
+        </v-btn>
+      </v-card-actions>
+      {{ originalQRCode }}
+      <div class="text-center">
+        <img :src="originalQRCode" alt="" />
+        <div id="canvas"></div>
+      </div>
+    </v-card>
   </v-layout>
 </template>
 
 <script>
-export default {}
+import QRCode from 'qrcode'
+
+export default {
+  data: () => ({
+    formData: {
+      ssid: '',
+      encryption: '',
+      key: ''
+    },
+    ssidRules: [(v) => !!v || 'SSID is required'],
+    keyRules: [(v) => !!v || 'KEY is required'],
+    encryptionRules: [(v) => !!v || 'Encryption is required'],
+    showErrorMessage: false,
+    version: 5,
+    errorCorrectionLevel: 'H',
+    originalQRCode: ''
+  }),
+  computed: {
+    encryptionItems() {
+      return ['WPA/WPA2', 'WEP', 'None']
+    },
+    existEmptyForm() {
+      return Object.values(this.formData).every((val) => val.length > 0)
+    },
+    wifiString() {
+      return `WIFI:S:${this.formData.ssid};T:${this.formData.encryption};P:${this.formData.key};;`
+    }
+  },
+  methods: {
+    createImage() {
+      this.showErrorMessage = false
+      if (!this.existEmptyForm) {
+        this.showErrorMessage = true
+      }
+      this.generateQR(this.wifiString)
+    },
+    async generateQR(string) {
+      await QRCode.toCanvas(
+        string,
+        {
+          errorCorrectionLevel: this.errorCorrectionLevel,
+          version: this.version
+        },
+        (err, canvas) => {
+          if (err) {
+            console.error(err)
+          }
+          const container = document.getElementById('canvas')
+          container.appendChild(canvas)
+        }
+      )
+    }
+  }
+}
 </script>
