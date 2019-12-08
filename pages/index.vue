2e61e8e2ad53e1db3bbd9dd731b61f6d9a8f3ca5
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-12-09 04:40:44 +0900

    pile images up

--- pages/index.vue
+++ pages/index.vue
@@ -11,19 +11,19 @@
             <v-col cols="12" md="4">
               <v-text-field
                 v-model="formData.ssid"
-                :rules="ssidRules"
+                :rules="rules.ssidRules"
                 label="SSID"
                 required
               />
               <v-select
                 v-model="formData.encryption"
                 :items="encryptionItems"
-                :rules="encryptionRules"
+                :rules="rules.encryptionRules"
                 label="Encryption"
               />
               <v-text-field
                 v-model="formData.key"
-                :rules="keyRules"
+                :rules="rules.keyRules"
                 label="KEY"
                 required
               />
@@ -44,10 +44,19 @@
           Get QRCode
         </v-btn>
       </v-card-actions>
-      {{ originalQRCode }}
+      <input
+        id="ufile"
+        @change="uploadImage"
+        name="ufile"
+        type="file"
+        accept="image/jpeg,image/png"
+      /><br />
       <div class="text-center">
-        <img :src="originalQRCode" alt="" />
-        <div id="canvas"></div>
+        <canvas
+          id="qrcode__canvas"
+          :width="canvasSize.width"
+          :height="canvasSize.height"
+        />
       </div>
     </v-card>
   </v-layout>
@@ -63,13 +72,24 @@ export default {
       encryption: '',
       key: ''
     },
-    ssidRules: [(v) => !!v || 'SSID is required'],
-    keyRules: [(v) => !!v || 'KEY is required'],
-    encryptionRules: [(v) => !!v || 'Encryption is required'],
+    rules: {
+      ssidRules: [(v) => !!v || 'SSID is required'],
+      keyRules: [(v) => !!v || 'KEY is required'],
+      encryptionRules: [(v) => !!v || 'Encryption is required']
+    },
+    canvasSize: {
+      height: 200,
+      width: 200
+    },
+    logoSize: {
+      height: 40,
+      width: 40
+    },
     showErrorMessage: false,
     version: 5,
     errorCorrectionLevel: 'H',
-    originalQRCode: ''
+    originalQRCode: '',
+    uploadImageBlobUrl: ''
   }),
   computed: {
     encryptionItems() {
@@ -83,29 +103,100 @@ export default {
     }
   },
   methods: {
+    /**
+     * アップロードした画像ファイルの blobUrl を取得する
+     */
+    uploadImage(value) {
+      this.image = value
+      const file = this.image.target.files[0]
+      this.uploadImageBlobUrl = window.URL.createObjectURL(file)
+    },
+    /**
+     * フォームがすべて入力されているとき、 QR コードを生成できる
+     */
     createImage() {
       this.showErrorMessage = false
       if (!this.existEmptyForm) {
         this.showErrorMessage = true
       }
-      this.generateQR(this.wifiString)
+      this.generateQR()
     },
-    async generateQR(string) {
-      await QRCode.toCanvas(
-        string,
-        {
-          errorCorrectionLevel: this.errorCorrectionLevel,
-          version: this.version
-        },
-        (err, canvas) => {
-          if (err) {
-            console.error(err)
-          }
-          const container = document.getElementById('canvas')
-          container.appendChild(canvas)
-        }
-      )
+    /**
+     * QR コードを生成する
+     */
+    async generateQR() {
+      await QRCode.toDataURL(this.wifiString)
+        .then((url) => {
+          this.originalQRCode = url
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+      // await QRCode.toCanvas(
+      //   string,
+      //   {
+      //     errorCorrectionLevel: this.errorCorrectionLevel,
+      //     version: this.version
+      //   },
+      //   (err, canvas) => {
+      //     if (err) {
+      //       console.error(err)
+      //     }
+      //     canvas.className = 'qrcode__canvas'
+      //     this.qrCodeCanvas = canvas
+      //     const container = document.querySelector('.qrcode__container')
+      //     container.appendChild(canvas)
+      //   }
+      // )
+      this.draw()
+    },
+    draw() {
+      const canvas = document.getElementById('qrcode__canvas')
+      if (!canvas || !canvas.getContext) {
+        return
+      }
+      const ctx = canvas.getContext('2d')
+      const qrCodeImage = new Image()
+      qrCodeImage.src = this.originalQRCode
+      qrCodeImage.onload = () => {
+        ctx.drawImage(
+          qrCodeImage,
+          0,
+          0,
+          this.canvasSize.width,
+          this.canvasSize.height
+        )
+      }
+      const logoImage = new Image()
+      logoImage.src = this.uploadImageBlobUrl
+      logoImage.onload = () => {
+        ctx.drawImage(
+          logoImage,
+          80,
+          80,
+          this.logoSize.width,
+          this.logoSize.height
+        )
+      }
     }
   }
 }
 </script>
+
+<style lang="scss">
+.qrcode__container {
+  position: relative;
+  width: 180px;
+  height: 180px;
+}
+
+.roxx {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  z-index: 1;
+}
+</style>
