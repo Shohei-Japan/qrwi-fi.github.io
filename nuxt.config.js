c136e83fe8548e65117c34b237da7d52eabe9554
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-12-08 20:18:59 +0900

    add deploy command

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,13 @@
 import colors from 'vuetify/es5/util/colors'
 
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
+
 export default {
+  ...routerBase,
   mode: 'spa',
   /*
    ** Headers of the page
